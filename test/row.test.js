2a53960b4bda84d9e3a7046f99c4b3ca187c4ee5
XXXXzy <381924237@qq.com>
2019-03-18 22:41:30 +0800

    col 代码优化

---
+++ test/row.test.js
@@ -0,0 +1,27 @@
+// const expect = chai.expect;
+// import Vue from 'vue'
+// import Row from '../src/layout/grid/row'
+// import Col from '../src/layout/grid/col'
+
+// Vue.config.productionTip = false
+// Vue.config.devtools = false
+
+// describe('Row', () => {
+
+//   it('存在.', () => {
+//     expect(Row).to.be.ok
+//   })
+
+//   it('接受 gutter 属性', () => {
+//     const div = document.createElement('div')
+//     document.appendChild(div)
+//     const RowConstructor = Vue.extend(Row)
+//     const ColConstructor = Vue.extend(Col)
+//     const col = new ColConstructor()
+//     const row = new RowConstructor({
+//       propsData: {
+//         guttor: 10
+//       }
+//     })
+//   })
+// })
