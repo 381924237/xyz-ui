00e23c727424f25a03cb68d88757f30d27c95cad
XXXXzy <381924237@qq.com>
2019-03-18 21:47:18 +0800

    初步完成响应式

--- src/layout/grid/col.vue
+++ src/layout/grid/col.vue
@@ -6,6 +6,17 @@
 
 
 <script>
+let validator = (value) => {
+  let keys = Object.keys(value)
+  let valid = true
+  keys.forEach(key => {
+    if(!['span','offset'].includes(key)){
+      valid = false
+    }
+  })
+  return valid
+}
+
 export default {
   name: 'YCol',
   data () {
@@ -14,11 +25,20 @@ export default {
     }
   },
   props: {
-    span: {
-      type: [Number,String]
-    },
-    offset: {
-      type: [Number,String]
+    span: {type: [Number,String]},
+    offset: {type: [Number,String]},
+    ipad: {type: Object,validator},
+    npc: {type: Object,validator},
+    pc: {type: Object,validator},
+    wpc: {type: Object,validator}
+  },
+  methods: {
+    createClasses(obj,str=''){
+      if(!obj) return []
+      let arr = []
+      if(obj.span) { arr.push(`col-${str}${obj.span}`) }
+      if(obj.offset) { arr.push(`offset-${str}${obj.offset}`)}
+      return arr 
     }
   },
   computed: {
@@ -30,10 +50,13 @@ export default {
       }
     },
     colClass(){
-      let {span,offset} = this
+      let { span,offset,ipad,npc,pc,wpc,createClasses } = this
       return [
-        span && `col-${span}`,
-        offset && `offset-${offset}`
+        ...createClasses({span,offset}),
+        ...createClasses(ipad,'ipad-'),
+        ...createClasses(npc,'npc-'),
+        ...createClasses(pc,'pc-'),
+        ...createClasses(wpc,'wpc-'),
       ]
     }
   }
@@ -53,6 +76,70 @@ export default {
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: ($n / 24) * 100%;
+      }
+    }
+
+    @media (min-width: 577px) {
+      $class-prefix: col-ipad-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) *100%;
+        }
+      }
+
+      $class-prefix: offset-ipad-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+
+    @media (min-width: 769px) {
+      $class-prefix: col-npc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) *100%;
+        }
+      }
+
+      $class-prefix: offset-npc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+
+    @media (min-width: 993px) {
+      $class-prefix: col-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) *100%;
+        }
+      }
+
+      $class-prefix: offset-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+
+    @media (min-width: 1201px) {
+      $class-prefix: col-wpc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) *100%;
+        }
+      }
+
+      $class-prefix: offset-wpc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
       }
     }
   }
