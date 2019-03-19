5c6c047ccbd24069bdf52dea0e96415b7a14f52b
XXXXzy <381924237@qq.com>
2019-03-19 14:23:53 +0800

    添加col、row测试

---
+++ test/col.test.js
@@ -0,0 +1,106 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Col from '../src/layout/grid/col'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Col',() => {
+  it('存在.',() => {
+    expect(Col).to.exist
+  })
+
+  it('接受 span 属性',() => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Col)
+    const vm = new Constructor({
+      propsData: {
+        span: 1
+      }
+    }).$mount(div)
+    const element = vm.$el
+    expect(element.classList.contains('col-1')).to.eq(true)
+    div.remove()
+    vm.$destroy()
+  })
+
+  it('接受 offset 属性',() => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Col)
+    const vm = new Constructor({
+      propsData: {
+        offset: 1
+      }
+    }).$mount(div)
+    const element = vm.$el
+    expect(element.classList.contains('offset-1')).to.eq(true)
+    div.remove()
+    vm.$destroy()
+  })
+
+  it('接受 ipad 属性',() => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Col)
+    const vm = new Constructor({
+      propsData: {
+        ipad: {span: 1, offset: 1}
+      }
+    }).$mount(div)
+    const element = vm.$el
+    expect(element.classList.contains('col-ipad-1')).to.eq(true)
+    expect(element.classList.contains('offset-ipad-1')).to.eq(true)
+    div.remove()
+    vm.$destroy()
+  })
+
+  it('接受 npc 属性',() => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Col)
+    const vm = new Constructor({
+      propsData: {
+        npc: {span: 1, offset: 1}
+      }
+    }).$mount(div)
+    const element = vm.$el
+    expect(element.classList.contains('col-npc-1')).to.eq(true)
+    expect(element.classList.contains('offset-npc-1')).to.eq(true)
+    div.remove()
+    vm.$destroy()
+  })
+
+  it('接受 pc 属性',() => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Col)
+    const vm = new Constructor({
+      propsData: {
+        pc: {span: 1, offset: 1}
+      }
+    }).$mount(div)
+    const element = vm.$el
+    expect(element.classList.contains('col-pc-1')).to.eq(true)
+    expect(element.classList.contains('offset-pc-1')).to.eq(true)
+    div.remove()
+    vm.$destroy()
+  })
+
+  it('接受 pc 属性',() => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Col)
+    const vm = new Constructor({
+      propsData: {
+        wpc: {span: 1, offset: 1}
+      }
+    }).$mount(div)
+    const element = vm.$el
+    expect(element.classList.contains('col-wpc-1')).to.eq(true)
+    expect(element.classList.contains('offset-wpc-1')).to.eq(true)
+    div.remove()
+    vm.$destroy()
+  })
+})
--- test/row.test.js
+++ test/row.test.js
@@ -1,27 +1,54 @@
-// const expect = chai.expect;
-// import Vue from 'vue'
-// import Row from '../src/layout/grid/row'
-// import Col from '../src/layout/grid/col'
+const expect = chai.expect;
+import Vue from 'vue'
+import Row from '../src/layout/grid/row'
+import Col from '../src/layout/grid/col'
 
-// Vue.config.productionTip = false
-// Vue.config.devtools = false
+Vue.config.productionTip = false
+Vue.config.devtools = false
 
-// describe('Row', () => {
+describe('Row', () => {
 
-//   it('存在.', () => {
-//     expect(Row).to.be.ok
-//   })
+  it('存在.', () => {
+    expect(Row).to.be.ok
+  })
 
-//   it('接受 gutter 属性', () => {
-//     const div = document.createElement('div')
-//     document.appendChild(div)
-//     const RowConstructor = Vue.extend(Row)
-//     const ColConstructor = Vue.extend(Col)
-//     const col = new ColConstructor()
-//     const row = new RowConstructor({
-//       propsData: {
-//         guttor: 10
-//       }
-//     })
-//   })
-// })
+  it('接受 gutter 属性', () => {
+    Vue.component('y-row', Row)
+    Vue.component('y-col', Col)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+      <y-row gutter="20">
+        <y-col span="12"></y-col>
+        <y-col span="12"></y-col>
+      </y-row>
+    `
+    const vm = new Vue({
+      el: div
+    })
+    setTimeout(() => {
+      const row = vm.$el.querySelector('.row')
+      expect(getComputedStyle(row).marginLeft).to.eq('-10px')
+      const cols = vm.$el.querySelectorAll('.col')
+      expect(getComputedStyle(cols[0]).paddingRight).to.eq('10px')
+      expect(getComputedStyle(cols[1]).paddingLeft).to.eq('10px')
+      vm.$el.remove()
+      vm.$destroy()
+    })
+  })
+
+  it('接受 align 属性', () => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Row)
+    const vm = new Constructor({
+      propsData: {
+        align: 'left'
+      }
+    }).$mount(div)
+    const element = vm.$el
+    expect(getComputedStyle(element).justifyContent).to.eq('flex-start')
+    div.remove()
+    vm.$destroy()
+  })
+})
